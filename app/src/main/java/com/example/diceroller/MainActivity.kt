7bd525c079e28a1d17ebc608364b07b63a039a7e
gabrielaliera <gliera2990@gmail.com>
2022-12-22 14:49:22 -0800

    Add lucky number conditional statement

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -27,10 +28,20 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val diceRoll2 = dice.roll()
+
+        val luckyNumber = 4
+        when(diceRoll){
+            luckyNumber ->Toast.makeText(this, "Lucky Number 4 rolled", Toast.LENGTH_SHORT).show()
+            1 -> Toast.makeText(this, "So sorry! You rolled a 1. Try again!", Toast.LENGTH_SHORT).show()
+            2 ->Toast.makeText(this, "Sadly, you rolled a 2. Try again!", Toast.LENGTH_SHORT).show()
+            3 -> Toast.makeText(this, "Unfortunately, you rolled a 3. Try again!", Toast.LENGTH_SHORT).show()
+            5 -> Toast.makeText(this, "Don't cry! You rolled a 5. Try again!", Toast.LENGTH_SHORT).show()
+            6 ->Toast.makeText(this, "Apologies! You rolled a 6. Try again!", Toast.LENGTH_SHORT).show()
+        }
         val resultTextView: TextView = findViewById(R.id.textView)
-        val resultTextView2: TextView = findViewById(R.id.textView2)
+       // val resultTextView2: TextView = findViewById(R.id.textView2)
         resultTextView.text = diceRoll.toString()
-        resultTextView2.text = diceRoll2.toString()
+        //resultTextView2.text = diceRoll2.toString()
 
     }
 }
